72cb0b5b8cb0922e3b88568b111bd29b8a34b75f
Indrajith C <indrajith.chandran@mozanta.com>
2023-09-26 12:28:42 +0530

    todo table added

--- src/plugins/todo/admin/src/pages/HomePage/index.js
+++ src/plugins/todo/admin/src/pages/HomePage/index.js
@@ -4,14 +4,9 @@
  *
  */
 
-import React, { useState} from "react";
+import React, { useState } from "react";
 
-import {
-  Layout,
-  BaseHeaderLayout,
-  ContentLayout,
-} from "@strapi/design-system";
-
+import { Layout, BaseHeaderLayout, ContentLayout } from "@strapi/design-system";
 
 import { EmptyStateLayout } from "@strapi/design-system";
 import { nanoid } from "nanoid";
@@ -20,16 +15,27 @@ import { Plus } from "@strapi/icons";
 import { Illo } from "../../components/Illo";
 
 import TodoModal from "../../components/TodoModal";
-
- 
+import TodoCount from "../../components/TodoCount";
+import TodoTable from "../../components/TodoTable";
 
 const HomePage = () => {
-
   const [todoData, setTodoData] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
   async function addTodo(data) {
     setTodoData([...todoData, { ...data, id: nanoid(), isDone: false }]);
+  }
+
+  async function toggleTodo(data) {
+    alert("Add Toggle Todo in API");
+  }
+
+  async function deleteTodo(data) {
+    alert("Add Delete Todo in API");
+  }
+
+  async function editTodo(id, data) {
+    alert("Add Edit Todo in API");
   }
 
   return (
@@ -40,32 +46,38 @@ const HomePage = () => {
         as="h2"
       />
       <ContentLayout>
-       {
-        todoData.length  === 0 ? 
-       (<EmptyStateLayout
-        icon={<Illo />}
-        content="You don't have any todos yet..."
-        action={
-          <Button
-            onClick={() => setShowModal(true)}
-            variant="secondary"
-            startIcon={<Plus />}
-          >
-            Add your first todo
-          </Button>
-        }
-      />):
-        <p> Count and Table</p>
-       }
-      </ContentLayout>
-      {
-        showModal? (
-          <TodoModal
-           setShowModal={setShowModal}
-           addTodo={addTodo}
+        {todoData.length === 0 ? (
+          <EmptyStateLayout
+            icon={<Illo />}
+            content="You don't have any todos yet..."
+            action={
+              <Button
+                onClick={() => setShowModal(true)}
+                variant="secondary"
+                startIcon={<Plus />}
+              >
+                Add your first todo
+              </Button>
+            }
           />
-        ):""
-      }
+        ) : (
+          <>
+            <TodoCount count={todoData.length} />
+            <TodoTable
+              todoData={todoData}
+              setShowModal={setShowModal}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+            />
+          </>
+        )}
+      </ContentLayout>
+      {showModal ? (
+        <TodoModal setShowModal={setShowModal} addTodo={addTodo} />
+      ) : (
+        ""
+      )}
     </Layout>
   );
 };
