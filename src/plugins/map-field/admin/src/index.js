b247e3622713c19dff9d709f469a8af5b7fed11b
Indrajith C <indrajith.chandran@mozanta.com>
2023-09-28 15:17:53 +0530

    map input and display

--- src/plugins/map-field/admin/src/index.js
+++ src/plugins/map-field/admin/src/index.js
@@ -3,30 +3,19 @@ import pluginPkg from '../../package.json';
 import pluginId from './pluginId';
 import Initializer from './components/Initializer';
 import PluginIcon from './components/PluginIcon';
+import MapInput from './components/MapInput';
+import mutateEditViewHook from "./mutateEditViewHook";
+import get from 'lodash/get';
+
+import cellFormatter from './components/cellFormatter';
 
 const name = pluginPkg.strapi.name;
 
 export default {
   register(app) {
-    app.addMenuLink({
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
-      },
-      Component: async () => {
-        const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
-
-        return component;
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
+    app.addFields({
+      type: pluginId,
+      Component: MapInput
     });
     app.registerPlugin({
       id: pluginId,
@@ -36,7 +25,64 @@ export default {
     });
   },
 
-  bootstrap(app) {},
+  bootstrap(app) {
+    app.registerHook(
+      "Admin/CM/pages/EditView/mutate-edit-view-layout",
+      mutateEditViewHook
+    );
+
+    app.registerHook(
+      'Admin/CM/pages/ListView/inject-column-in-table',
+      ({ displayedHeaders, layout }) => {
+        console.log({ displayedHeaders, layout })
+        const isFieldLocalized = get(
+          layout,
+          'contentType.pluginOptions.i18n.localized',
+          false
+        );
+        // if (!isFieldLocalized) {
+        //   return { displayedHeaders, layout };
+        // }
+
+        console.log({
+          layout,
+          displayedHeaders: [
+            ...displayedHeaders,
+            {
+              key: '__locale_key__', // Needed for the table
+              fieldSchema: { type: 'string' }, // Schema of the attribute
+              metadatas: {
+                label: 'Content available in', // Label of the header,
+                sortable: true | false, // Define if the column is sortable
+              }, // Metadatas for the label
+              // Name of the key in the data we will display
+              name: 'locales',
+              // Custom renderer: props => Object.keys(props).map(key => <p key={key}>key</p>)
+              cellFormatter,
+            },
+          ],
+        })
+        return {
+          layout,
+          displayedHeaders: [
+            ...displayedHeaders,
+            {
+              key: '__locale_key__', // Needed for the table
+              fieldSchema: { type: 'string' }, // Schema of the attribute
+              metadatas: {
+                label: 'Content available in', // Label of the header,
+                sortable: true | false, // Define if the column is sortable
+              }, // Metadatas for the label
+              // Name of the key in the data we will display
+              name: 'locales',
+              // Custom renderer: props => Object.keys(props).map(key => <p key={key}>key</p>)
+              cellFormatter,
+            },
+          ],
+        };
+      }
+    );
+  },
   async registerTrads({ locales }) {
     const importedTrads = await Promise.all(
       locales.map((locale) => {
